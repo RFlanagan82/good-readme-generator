906ae28b4e3931908a484777d7f2b0b12d5975cc
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-09-23 15:49:08 -0400

    added in constant variables on index.js for connecting the generateMarkdown.js file to it, adding the inquire package and fs package.

---
+++ index.js
@@ -0,0 +1,26 @@
+//Need to connect generateMarkdown.js file to here
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+//Need to connect inquirer package to application
+const inquirer = require("inquirer");
+
+//Need to connect built-in filesystem package to application
+const fs = require("fs");
+
+
+// array of questions for user
+const questions = [
+    
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
