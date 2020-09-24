25d1e6180188b346365410611f4545947600c004
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-09-24 06:51:09 -0400

    Troubleshooting the license badges in generateMarkdown.md

---
+++ README.md
@@ -0,0 +1,48 @@
+
+    
+    # Title
+    Good Readme Generator
+
+    ## Badge
+    undefined
+  
+    ## Table of Contents
+    *[Description](#description)
+    *[Installation requirements](#installation)
+    *[Usage Information](#usage)
+    *[Credits / Contributions](#contributors)
+    *[Resources](#resources)
+    *[Tests](#tests)
+    *[License](#license)
+    *[Questions](#questions)
+
+    ## Description
+    To create a command-line application that dynamically generates a professional README.md from a user's input using the Inquirer Package.
+
+    ## Installation Requirements
+    Steps ABC
+
+    ## Usage Information
+    These are referenced in the Submission Requirements section within [instructions.md](https://github.com/RFlanagan82/good-readme-generator/blob/master/instructions.md)
+
+    ## Credits & Contributions
+    On 9-24-2020, I worked with tutor Andrew Hardemon - who helped me create dynamically generated License links and helped me add my DS_Store file.
+
+    ## Resources
+    *[Markdown License badges](https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba) 
+
+    ## Tests
+      *[JavaScript String Link Method](https://www.w3schools.com/jsref/jsref_link.asp) 
+
+    ## License
+    Boost 1.0
+    
+
+    ## Questions
+    Contact:
+
+    Github: [RFlanagan82](https://github.com/RFlanagan82)
+
+    Email: [flanagan.ryan@gmail.com](mailto:flanagan.ryan@gmail.com)
+  
+    
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,29 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
     let licenseText = "";
-    let licenseLink = licenseText
     if (data.license === "APACHE 2.0"){
-        licenseLink = str.link("[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)");
+        data.badge = str.link("[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)");
+    } else if (data.license === "Boost 1.0"){
+        data.badge = str.link("[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)");
+    } else if (data.license === "BSD"){
+        data.badge = str.link("[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)");
+    } else if (data.license === "Eclipse 1.0"){
+        data.badge = str.link("[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)");
+    } else if (data.license === "IBM 1.0"){
+        data.badge = str.link("[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)");
+    } else if (data.license === "MIT"){
+        data.badge = str.link("![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)");
+    } else if (data.license === "None"){
+        data.badge = "None"
     }
 
     return `
     
     # Title
     ${data.Title}
+
+    ## Badge
+    ${data.Badge}
   
     ## Table of Contents
     *[Description](#description)
